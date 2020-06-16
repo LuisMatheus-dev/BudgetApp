4318e3a2e7ee84bffc1e7c89c7b996f8f48db96d
LuisMatheus.dev <luismatheusbs.2016@gmail.com>
2020-06-15 21:57:17 -0300

    :rocket: [feat] Cadastro de lucros e gastos
    Implementado a captura dos inputs utilizando controladores de UI,
    e refatorando o codigo para deixa-lo reutilizavel e mais modular

--- app.js
+++ app.js
@@ -12,14 +12,50 @@ var budgetController = (function() {
     }
 })();
 
-var controller = (function(budgetCtrl, UICtrl) {
+var UIController = (function() {
 
-    var z = budgetCtrl.publicTest(5);
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
 
     return {
-        anotherPuplic: function() {
-            console.log(z);
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            };
+        },
+
+        getDOMStrings: function() {
+            return DOMStrings;
         }
-    }
+    };
+
+}());
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var DOM = UICtrl.getDOMStrings();
+
+    var ctrlAddItem = function() {
+        
+        var input = UICtrl.getInput();
+        console.log(input);
+    };
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem) 
+
+    document.addEventListener('keypress', function(event) {
+        
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
+    });
+    
 
 }(budgetController, UIController));
