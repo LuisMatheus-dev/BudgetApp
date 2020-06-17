930d4049bcda70554a7ae5821c7b1e2c2248b058
LuisMatheus.dev <luismatheusbs.2016@gmail.com>
2020-06-16 22:27:59 -0300

    :rocket: [feat] Adicionado Atualização na UI

--- app.js
+++ app.js
@@ -56,7 +56,10 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
+
     };
 
     return {
@@ -66,6 +69,26 @@ var UIController = (function() {
                 description: document.querySelector(DOMStrings.inputDescription).value,
                 value: document.querySelector(DOMStrings.inputValue).value
             };
+        },
+
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+
+            if(type === 'inc') {
+                element = DOMStrings.incomeContainer;
+
+                html ='<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            } else if (type === 'exp') {
+                element = DOMStrings.expensesContainer;
+
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);   
+
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml)
         },
 
         getDOMStrings: function() {
@@ -96,6 +119,8 @@ var controller = (function(budgetCtrl, UICtrl) {
          input = UICtrl.getInput();
 
          newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+    
+        UICtrl.addListItem(newItem, input.type);
     };
 
     return {
